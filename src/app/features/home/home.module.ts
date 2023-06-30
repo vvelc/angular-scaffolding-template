455ec6fa14a9261e1d239cafb0629d0b030fb5c8
Victor Velazquez Cid <victorvelazquezcid@gmail.com>
2023-06-30 11:25:59 -0400

    Add files

---
+++ src/app/features/home/home.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './pages/home.component';
+import { } from 'primeng/button';
+
+@NgModule({
+  imports: [],
+  exports: [HomeComponent],
+  declarations: [HomeComponent],
+  providers: [],
+})
+export class HomeModule {}
